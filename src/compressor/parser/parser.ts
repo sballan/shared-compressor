c5dc2deb73695214331a94d782457e9cd771300e
Samuel Ballan <sgb4622@gmail.com>
2017-01-23 21:04:28 -0500

    scanner is pretty much set up

--- src/compressor/parser/parser.ts
+++ src/compressor/parser/parser.ts
@@ -1,179 +1,64 @@
 import {
 	Token, Expr, isChar,
 	Terminal, Separator, Char,
-	Nonterminal, Word, Clause, Sentence, Paragraph
+	Nonterminal, Word, Clause, Sentence, Paragraph, Corpus
 } from '../tokens';
 
+import { Scanner } from './scanner'
+
+
 export class Parser {
+	private scanner: Scanner;
 	private input: string[];
 	tokens: Token<Expr>[];
 
 
 	constructor(input: string) { 
+		this.scanner = new Scanner(input);
 		this.input = input.split('').reverse();
 	}
 
 	public run() {
-		console.log("About to scan: \n", this.tokens)
-		this.scan();
 		console.log("About to parse words: \n", this.tokens)
 		this.words();
 		console.log("About to parse clauses: \n", this.tokens)
 		this.clauses();
-		console.log("About to parse sentences: \n", this.tokens[2].toString())
+		console.log("About to parse sentences: \n", this.tokens)
 		// this.sentences()
 		// console.log("About to parse paragraphs: \n")
 		// this.paragraphs();
 	}
 
-	scan() {
-		this.tokens = [];
-
-		this.input.forEach(c => {
-			let token;
-			if (isChar(c)) token = Token.create<Char>(c, Char)	
-			else token = Token.create<Separator>(c, Separator)
-			this.tokens.push(token)
-		})
-	}
-
-	private words() {
-		const words: Array<Token<Word | Terminal>> = [];
-		const length = this.tokens.length;
-
-		// buffer for word being parsed
-		let buffer: Token<Terminal>[] = [];
-
-		while (this.tokens.length > 0) {
-			const token = this.tokens.pop();
-
-			if (!(token.value instanceof Terminal)) {
-				throw Error('Tried to parse Nonterminal as Word')
-			}
-
-			if (token.value instanceof Separator) {
-				if (buffer.length > 0) {
-					const wordToken = Token.create<Word>(buffer, Word) as Token<Word>;
-					words.push(wordToken);
-				}
-				words.push(token as Token<Separator>);
-				buffer = [];
-			} else if (token.value instanceof Char) {
-				buffer.push(token as Token<Char>);
-			} else {
-				console.log(token);
-				throw Error('Wrong arguments to words()')	
-			}
-
-		}
-	
-		return this.tokens = words.reverse();
-
+	private words(): Token<Word>[] {
+		const wordTokens = this.scanner.words();
+		
+		return wordTokens;
 	}
 
 	private clauses() {
-		const clauses: Array<Token<Clause | Terminal>> = [];
-		const length = this.tokens.length;
-
-		let buffer: Array<Token<Word | Terminal>> = [];
-
-		while (this.tokens.length > 0) {
-			const token = this.tokens.pop();
-
-			if (!(token.value instanceof Word) && !(token.value instanceof Terminal)) {
-				throw Error('Tried to parse Phrase as Word')
-			}
-
-			if (token.value instanceof Separator) {
-				if (token.value.literal === '.' || token.value.literal === ',') {
-					if (buffer.length > 0) {
-						const clauseToken = Token.create<Clause>(buffer, Clause) as Token<Clause>;
-						clauses.push(clauseToken);
-						buffer = [];
-					}
-					clauses.push(token as Token<Separator>);
-				} else {
-					buffer.push(token as Token<Separator>)
-				}			
-			} else if (token.value instanceof Word) {
-				buffer.push(token as Token<Word>);
-			} else {
-				console.log(token);
-				throw Error('Wrong arguments to words()')	
-			}
-
-		}
-
-		if (buffer.length > 0) {
-			const clauseToken = Token.create<Clause>(buffer, Clause) as Token<Clause>;
-			clauses.push(clauseToken);
-		}
-
-		return this.tokens = clauses.reverse();
+		const clauseTokens = this.scanner.clauses();
+		
+		return clauseTokens;
 
 	}
 
-	private sentences() {
-		const sentences: Array<Token<Sentence | Terminal>> = [];
-		const length = this.tokens.length;
+	private sentences() : Token<Sentence>[] {
+		const sentenceTokens = this.scanner.sentences();
+		
+		return sentenceTokens;
+	}
 
-		let buffer: Array<Token<Clause | Terminal>> = [];
-
-		while (this.tokens.length > 0) {
-			const token = this.tokens.pop();
-
-			if (!(token.value instanceof Clause) && !(token.value instanceof Terminal)) {
-				throw Error('Tried to parse Sentence as Phrase')
-			}
-
-			switch (true) {
-				case token.value.literal === '.':
-					const sentenceToken = Token.create<Sentence>(buffer, Sentence) as Token<Sentence>;
-					sentences.push(sentenceToken, token as Token<Terminal>);	
-						break;
-				case isChar(token.value.literal):
-					buffer.push(token as Token<Terminal>);
-					break;
-				default:
-					sentences.push(token as Token<Terminal>);
-					break;
-			}
-
-		}
-
-		return this.tokens = sentences.reverse();
+	private paragraphs() : Token<Paragraph>[]{
+		const paragraphTokens = this.scanner.paragraphs();
+		
+		return paragraphTokens;
 
 	}
 
-	private paragraphs() {
-		const paragraphs: Array<Token<Paragraph | Terminal>> = [];
-		const length = this.tokens.length;
-
-		let buffer: Array<Token<Sentence | Terminal>> = [];
-
-		while (this.tokens.length > 0) {
-			const token = this.tokens.pop();
-
-			if (!(token instanceof Sentence) && !(token instanceof Terminal)) {
-				throw Error('Tried to parse Paragraph as Sentence')
-			}
-
-			switch (true) {
-				case token.value.literal === '\n\n':
-					const paragraphToken = Token.create<Paragraph>(buffer, Paragraph) as Token<Paragraph>;
-					paragraphs.push(paragraphToken, token as Token<Terminal>);	
-					break;
-				case isChar(token.value.literal):
-					buffer.push(token as Token<Terminal>);
-					break;
-				default:
-					paragraphs.push(token as Token<Terminal>);
-					break;
-			}
-
-		}
-
-		return this.tokens = paragraphs.reverse();
+	private corpus() : Token<Corpus>{
+		const corpusToken = this.scanner.corpus();
+		
+		return corpusToken;
 
 	}
 
---
+++ src/compressor/parser/scanner.ts
@@ -0,0 +1,203 @@
+import {
+	Token, Expr,
+	Terminal, Separator, Char,
+	Nonterminal, Word, Clause, Sentence, Paragraph, Corpus
+} from '../tokens'
+
+function isChar(value: string): boolean {
+	return value.match(/[A-Za-z0-9]/) !== null;
+}
+
+export class Scanner {
+	// public cache: Array<Token<Word | Separator>>;
+	private buffer: Token<Expr>[] = [];
+	private tokens: Array<Token<Expr>> = [];
+	private current: Token<Expr>;
+	public inputTokens: Token<Expr>[] = [];
+
+	constructor(public inputString: string) { 
+		this.terminals(inputString)
+	}
+
+	terminals(inputString: string = this.inputString) {
+		inputString.split('').forEach(c => {
+			let token;
+			if (isChar(c)) token = Token.create<Char>(c, Char)	
+			else token = Token.create<Separator>(c, Separator)
+			this.inputTokens.push(token)
+		})
+	}
+	
+	words(): Token<Word>[] {
+		this.tokens = []
+		while (this.inputTokens.length > 0) {
+			this.current = this.inputTokens.pop();
+
+			if (!(this.current.value instanceof Terminal)) {
+				throw Error('Tried to parse Nonterminal as Word')
+			}
+
+			if (this.current.value instanceof Separator) {
+				if (this.buffer.length > 0) {
+					const wordToken = Token.create<Word>(this.buffer, Word);
+					this.tokens.push(wordToken);
+				}
+				this.tokens.push(this.current as Token<Separator>);
+				this.buffer = [];
+			} else if (this.current.value instanceof Char) {
+				this.buffer.push(this.current as Token<Char>);
+			} else {
+				console.log(this.current);
+				throw Error('Wrong arguments to words()')	
+			}
+
+		}
+	
+		return this.tokens.reverse() as Token<Word>[];
+
+	}
+
+	clauses() {
+		this.tokens = [];
+		while (this.inputTokens.length > 0) {
+			const current = this.inputTokens.pop();
+
+			if (!(current.value instanceof Word) && !(current.value instanceof Separator)) {
+				throw Error('Tried to parse Phrase as Word')
+			}
+
+			if (current.value instanceof Separator) {
+				if (current.value.literal === '.' || current.value.literal === ',') {
+					if (this.buffer.length > 0) {
+						const clauseToken = Token.create<Clause>(this.buffer, Clause);
+						this.tokens.push(clauseToken);
+						this.buffer = [];
+					}
+					this.buffer.push(current as Token<Separator>)
+				}			
+			} else if (current.value instanceof Word) {
+				this.buffer.push(current as Token<Word>);
+			} else {
+				console.log(current);
+				throw Error('Wrong arguments to words()')	
+			}
+
+		}
+
+		if (this.buffer.length > 0) {
+			const clauseToken = Token.create<Clause>(this.buffer, Clause) as Token<Clause>;
+			this.tokens.push(clauseToken);
+			this.buffer = [];
+		}
+
+		return this.tokens.reverse() as Token<Clause>[];
+
+	}
+
+	sentences() {
+		this.tokens = [];
+		while (this.inputTokens.length > 0) {
+			const current = this.inputTokens.pop();
+
+			if (!(current.value instanceof Clause) && !(current.value instanceof Separator)) {
+				throw Error('Tried to parse Phrase as Word')
+			}
+
+			if (current.value instanceof Separator) {
+				if (current.value.literal === '.') {
+					if (this.buffer.length > 0) {
+						const sentenceToken = Token.create<Sentence>(this.buffer, Sentence);
+						this.tokens.push(sentenceToken);
+						this.buffer = [];
+					}
+					this.buffer.push(current as Token<Separator>)
+				}			
+			} else if (current.value instanceof Clause) {
+				this.buffer.push(current as Token<Clause>);
+			} else {
+				console.log(current);
+				throw Error('Wrong arguments to sentences()')	
+			}
+
+		}
+
+		if (this.buffer.length > 0) {
+			const clauseToken = Token.create<Sentence>(this.buffer, Sentence) as Token<Sentence>;
+			this.tokens.push(clauseToken);
+			this.buffer = [];
+		}
+
+		return this.tokens.reverse() as Token<Sentence>[];
+
+	}
+
+	paragraphs() {
+		this.tokens = [];
+		while (this.inputTokens.length > 0) {
+			const current = this.inputTokens.pop();
+
+			if (!(current.value instanceof Sentence) && !(current.value instanceof Separator)) {
+				throw Error('Tried to parse Paragraph as Sentence')
+			}
+
+			if (current.value instanceof Separator) {
+				if (current.value.literal === '\n\n') {
+					if (this.buffer.length > 0) {
+						const paragraphToken = Token.create<Paragraph>(this.buffer, Sentence);
+						this.tokens.push(paragraphToken);
+						this.buffer = [];
+					}
+					this.buffer.push(current as Token<Separator>)
+				}			
+			} else if (current.value instanceof Sentence) {
+				this.buffer.push(current as Token<Sentence>);
+			} else {
+				console.log(current);
+				throw Error('Wrong arguments to paragraphs()')	
+			}
+
+		}
+
+		if (this.buffer.length > 0) {
+			const paragraphToken = Token.create<Paragraph>(this.buffer, Paragraph) as Token<Paragraph>;
+			this.tokens.push(paragraphToken);
+			this.buffer = [];
+		}
+
+		return this.tokens.reverse() as Token<Paragraph>[];
+
+	}
+
+	corpus() {
+		this.tokens = [];
+		while (this.inputTokens.length > 0) {
+			const current = this.inputTokens.pop();
+
+			if (!(current.value instanceof Paragraph) && !(current.value instanceof Separator)) {
+				throw Error('Tried to parse Corpus as Paragraph')
+			}
+
+			if (current.value instanceof Paragraph) {
+				this.buffer.push(current as Token<Paragraph>);
+			} else if (current.value instanceof Separator) {
+				this.buffer.push(current as Token<Separator>);
+			} else {
+				throw Error('Wrong arguments to corpus()')	
+			}
+
+		}
+
+		if (this.buffer.length > 0) {
+			const corpusToken = Token.create<Paragraph>(this.buffer, Corpus) as Token<Paragraph>;
+			this.tokens.push(corpusToken);
+			this.buffer = [];
+		} else {
+			throw Error("Buffer should not be empty")
+		}
+
+		if(this.tokens.length>1) throw Error("Buffer overflow")
+
+		return this.tokens.reverse().pop() as Token<Corpus>;
+
+	}
+}
